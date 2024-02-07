210abdf1ef64f5d9b6b4f26903018588240e15fa
santos rafaelo <santoschrist1234@gmail.com>
2024-02-07 10:52:03 +0200

    feat:updated controller,service and config file

--- src/main/java/com/example/Schoolwebsitebackend/Model/Role.java
+++ src/main/java/com/example/Schoolwebsitebackend/Model/Role.java
@@ -1,4 +1,29 @@
 package com.example.Schoolwebsitebackend.Model;
 
-public class Role {
+import jakarta.persistence.Entity;
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Id;
+@Entity
+public class Role implements GrantedAuthority {
+    @Id
+    private String authority;
+
+    public Role(String authority) {
+        this.authority = authority;
+    }
+
+    public Role() {
+    }
+
+
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
 }
